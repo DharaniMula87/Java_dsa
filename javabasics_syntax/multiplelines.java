ea7d90a57c85ade514686210eaa9b11ed06682ff
DharaniMula87 <dharanimula2006@gmail.com>
2026-07-17 21:13:42 +0530

    Multiple lines printing

---
+++ javabasics_syntax/multiplelines.java
@@ -0,0 +1,10 @@
+ //Print multiple lines multiplelines
+class multiplelines
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello");
+        System.out.println("I am M.Dharani");
+        System.out.println("Pursing 4th year B.Tech");
+    }
+}
