54ac7cee724bb87f670554641ef23c11d683a8de
DharaniMula87 <dharanimula2006@gmail.com>
2026-07-17 21:48:13 +0530

    Tabular data printing

---
+++ javabasics_syntax/tabulardata.java
@@ -0,0 +1,10 @@
+//Print tabular data
+class tabulardata
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Name\tBranch\tYear"); 
+        System.out.println("Dharani\tIT\t4");
+        System.out.println("Rajini\tCSE\t4");
+    }
+}
