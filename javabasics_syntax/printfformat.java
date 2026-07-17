665522f6eeda04824ba8e379d2ad889a06ceb7d8
DharaniMula87 <dharanimula2006@gmail.com>
2026-07-17 21:55:56 +0530

    printf() formatting

---
+++ javabasics_syntax/printfformat.java
@@ -0,0 +1,11 @@
+//Print formatted output using printf()
+class printfformat
+{
+    public static void main(String[] args)
+    {
+        String name="Dharani";
+        int age=20;
+        System.out.printf("Name: %s\n",name);
+        System.out.printf("Age: %d",age);
+    }
+}
