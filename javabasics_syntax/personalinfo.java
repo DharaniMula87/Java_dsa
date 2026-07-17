29e6135c3ec0ef5acab53fe101cd21d9a15a6c0d
DharaniMula87 <dharanimula2006@gmail.com>
2026-07-17 21:58:39 +0530

    displaying personal info

---
+++ javabasics_syntax/personalinfo.java
@@ -0,0 +1,9 @@
+//Display personal information
+class personalinfo
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Name: "+"Dharani");
+        System.out.println("Age: "+19);
+    }
+}
