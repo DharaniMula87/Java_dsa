d84fa67df493ac56e1bdf20f32ce12cc7b29308e
DharaniMula87 <dharanimula2006@gmail.com>
2026-07-17 21:24:44 +0530

    Printing the square

---
+++ javabasics_syntax/printsquare.java
@@ -0,0 +1,11 @@
+//Print a square
+class printsquare
+{
+    public static void main(String[] args)
+    {
+        System.out.println("*********");
+        System.out.println("*********");
+        System.out.println("*********");
+        System.out.println("*********");
+    }
+}
