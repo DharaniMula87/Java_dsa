b94d81460790ffb87c9d460523b6aeaa3c15e1f2
DharaniMula87 <dharanimula2006@gmail.com>
2026-07-17 21:32:01 +0530

    Right angle triangle

---
+++ javabasics_syntax/righttriangle.java
@@ -0,0 +1,12 @@
+//Print a right triangle
+class righttriangle
+{
+    public static void main(String[] args)
+    {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+    }
+}
