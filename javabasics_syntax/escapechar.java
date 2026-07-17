e42f4940e60e213546b39ae216b426857bc17973
DharaniMula87 <dharanimula2006@gmail.com>
2026-07-17 21:40:13 +0530

    Escape characters

---
+++ javabasics_syntax/escapechar.java
@@ -0,0 +1,11 @@
+//Print escape characters
+class escapechar
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello\tWorld");  //tab space
+        System.out.println("Java\nprogramming"); //next line
+        System.out.println("\"Dharani\"");   //Printing the name in double quotes
+        System.out.println("C:\\Dharani\\java\\programming");  //Without writing the double quotes we can print the slashes
+    }
+}
