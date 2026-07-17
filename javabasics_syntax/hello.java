2ed6db82a6299449b23d752a8c2cf04857737860
DharaniMula87 <dharanimula2006@gmail.com>
2026-07-17 20:59:23 +0530

    Initial commit

---
+++ javabasics_syntax/hello.java
@@ -0,0 +1,22 @@
+//Hello World
+
+/*class hello
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+    }
+}
+*/
+
+
+// with only statoc block without main the programs runs in older versions of java6 after java6 the java demands to run with main.
+class hello
+{
+static
+{
+System.out.println("Hello World");
+}
+public static void main(String[] args)
+    {}
+}
