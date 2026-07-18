ecc6912df876e10d792954da9aed4ee51dfb61f9
DharaniMula87 <dharanimula2006@gmail.com>
2026-07-18 20:31:05 +0530

    Area of the rectangle

---
+++ javabasics_syntax/rectarea.java
@@ -0,0 +1,11 @@
+//Calculate area of rectangle
+class rectarea
+{
+    public static void main(String[] args)
+    {
+        int length=10;
+        int width=7;
+        int area=length*width;
+        System.out.println("Area of the rectangle is "+area);
+    }
+}
