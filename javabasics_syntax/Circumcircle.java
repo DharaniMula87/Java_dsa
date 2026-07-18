857ce9c5ec6e90ac1963aadc0e70cbf608c609db
DharaniMula87 <dharanimula2006@gmail.com>
2026-07-18 20:40:00 +0530

    Circumference of a circle

---
+++ javabasics_syntax/Circumcircle.java
@@ -0,0 +1,11 @@
+//Circumference of circle 
+//formulas: 2*pi*r  and pi*diameter
+class Circumcircle
+{
+    public static void main(String[] args)
+    {
+        int radius=7;
+        double circumference=2*3.14*radius;
+        System.out.println("Circumference of a circle is "+circumference);
+    }
+}
