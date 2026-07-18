b8eeb1d008ac8a0efc774522a5a17663ccad4f29
DharaniMula87 <dharanimula2006@gmail.com>
2026-07-18 20:34:22 +0530

    Area of the circle

---
+++ javabasics_syntax/circlearea.java
@@ -0,0 +1,10 @@
+//Area of circle
+class circlearea
+{
+    public static void main(String[] args)
+    {
+        int radius=4;
+        double area=3.14*radius*radius;
+        System.out.println("Area of the circle is "+area);
+    }
+}
