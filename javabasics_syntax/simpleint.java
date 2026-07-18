de4d033aec9943e21e80e3da6f2ed975856eec66
DharaniMula87 <dharanimula2006@gmail.com>
2026-07-18 20:44:25 +0530

    Simple Interest

---
+++ javabasics_syntax/simpleint.java
@@ -0,0 +1,12 @@
+//Simple Interest
+class simpleint
+{
+    public static void main(String[] args)
+    {
+        int amount=20000;
+        int time=3;
+        int rate=4;
+        double si=(amount*time*rate)/100;
+        System.out.println("Simple Interest is "+si);
+    }
+}
