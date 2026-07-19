b594658a8e84a963f367959a7813817cba0a03ac
DharaniMula87 <dharanimula2006@gmail.com>
2026-07-19 19:25:37 +0530

    Explicit casting

---
+++ javabasics_syntax/explicit.java
@@ -0,0 +1,11 @@
+//Explicit casting
+//explicit type casting is conversion of large datatype to small datatype
+class explicit
+{
+    public static void main(String[] args)
+    {
+        double d=10.56;
+        int n=(int)d;
+        System.out.println("Explicit Type Casting: "+n);
+    }
+}
