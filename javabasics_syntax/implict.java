717b795df33350c927e1588202e3a7360eff743f
DharaniMula87 <dharanimula2006@gmail.com>
2026-07-19 19:07:58 +0530

    Implicit casting

---
+++ javabasics_syntax/implict.java
@@ -0,0 +1,11 @@
+//Implicit casting
+// convert the small datatype to large datatype by user.
+class implict
+{
+    public static void main(String[] args)
+    {
+        int a=20;
+        double d=a;
+        System.out.println("Implicit Type Casting: "+d); 
+    }
+}
