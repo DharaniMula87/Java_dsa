6a472042c40d29b96b4a570a2d96fcd6594d204a
DharaniMula87 <dharanimula2006@gmail.com>
2026-07-20 17:33:13 +0530

    Double to Integer conversion

---
+++ javabasics_syntax/doubletoint.java
@@ -0,0 +1,9 @@
+//Double to integer
+class doubletoint
+{
+    public static void main(String[] args)
+    {
+        double d=75.98348;
+        System.out.println("Double to Int conversion: "+(int)d);
+    }
+}
