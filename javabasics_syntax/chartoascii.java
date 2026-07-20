78fff306379deb61357a67c921c35026ef8301f8
DharaniMula87 <dharanimula2006@gmail.com>
2026-07-20 17:38:42 +0530

    Character to int

---
+++ javabasics_syntax/chartoascii.java
@@ -0,0 +1,10 @@
+//Character to ASCII
+class chartoascii
+{
+    public static void main(String[] args)
+    {
+        char c='D';
+        int n=(int) c;
+        System.out.println("char D into int :" +n);
+    }
+}
