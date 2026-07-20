113c87ffd731b4dfcb966ba4764ca7ec762836e5
DharaniMula87 <dharanimula2006@gmail.com>
2026-07-20 17:50:59 +0530

    Boolean Demonstration

---
+++ javabasics_syntax/booleandemonstration.java
@@ -0,0 +1,11 @@
+//Boolean demonstration
+//boolean is the datatype where we store only true and false values
+class booleandemonstration
+{
+    public static void main(String[] args)
+    {
+        boolean a=true;
+        boolean b=false;
+        System.out.println("A: "+a+"\nB: "+b);
+    }
+}
