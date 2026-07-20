3bf3bf51f25e9e72e9e6ceca6264d96f3a58bf36
DharaniMula87 <dharanimula2006@gmail.com>
2026-07-20 17:41:41 +0530

    Ascii to char conversion

---
+++ javabasics_syntax/asciitochar.java
@@ -0,0 +1,10 @@
+//ASCII to character
+class asciitochar
+{
+    public static void main(String[] args)
+    {
+        int n=87;
+        char c=(char)n;
+        System.out.println("87 into the character :"+c);
+    }
+}
