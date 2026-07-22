2019675dced694cc1c92122b86368a3df5b584f6
DharaniMula87 <dharanimula2006@gmail.com>
2026-07-22 19:19:25 +0530

    Multiplication

---
+++ Operators/multiplication.java
@@ -0,0 +1,11 @@
+//Multiplication
+class multiplication 
+{
+    public static void main(String[] args)
+    {
+        int a=12;
+        int b=7;
+
+        System.out.println("Multiplication of a and b: "+(a*b));
+    }    
+}
