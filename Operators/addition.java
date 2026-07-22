c548e099e4098f5889d46482bda6259a41ad7774
DharaniMula87 <dharanimula2006@gmail.com>
2026-07-22 19:12:39 +0530

    Addition

---
+++ Operators/addition.java
@@ -0,0 +1,16 @@
+//Addition
+
+class addition
+{
+    public static void main(String[] args)
+    {
+        int a=18;
+        int b=4;
+
+        float c=34.87f;
+        float d=67.02f;
+
+        System.out.println("Addition of Integers a and b: "+(a+b));
+        System.out.println("Addition of floating numbers c and d: "+(c+d));
+    }
+}
