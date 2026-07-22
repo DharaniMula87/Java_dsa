5b678455f364d86bd318fcb62ba4eeb61e5c7549
DharaniMula87 <dharanimula2006@gmail.com>
2026-07-22 19:22:08 +0530

    Division

---
+++ Operators/division.java
@@ -0,0 +1,11 @@
+//Division
+class division 
+{
+    public static void main(String[] args)
+    {
+        int a=35;
+        int b=7;
+        int res=a/b;
+        System.out.println("Division of a and b is "+res);
+    }    
+}
