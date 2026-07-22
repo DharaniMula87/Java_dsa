168b294e7cfc57141fe2048963febfbbd4fbc8fb
DharaniMula87 <dharanimula2006@gmail.com>
2026-07-22 19:16:43 +0530

    Substraction

---
+++ Operators/substraction.java
@@ -0,0 +1,15 @@
+//Substraction
+class substraction 
+{
+    public static void main(String[] args)
+    {
+        int a=76;
+        int b=71;
+
+        double d=45.9887778;
+        double f=87.96556;
+
+        System.out.println("Substraction of Integers a and b: "+(a+b));
+        System.out.println("Substraction of Double d and f: "+(d+f));
+    }   
+}
