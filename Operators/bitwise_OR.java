f1a6c448c269a17ff8451805a361ca818df8db02
DharaniMula87 <dharanimula2006@gmail.com>
2026-08-08 11:54:00 +0530

    Bitwise_OR

---
+++ Operators/bitwise_OR.java
@@ -0,0 +1,11 @@
+//Bitwise OR
+public class bitwise_OR
+{
+    public static void main(String[] args)
+    {
+        int a=12;
+        int b=7;
+        int r=a|b;
+        System.out.println(r);
+    }
+}
