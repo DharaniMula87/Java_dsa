aedd0c8a75448e9cfa4cd81b4637227d5fee4d22
DharaniMula87 <dharanimula2006@gmail.com>
2026-08-08 09:55:27 +0530

    increment and decrement

---
+++ Operators/decrement.java
@@ -0,0 +1,10 @@
+//decrement
+public class decrement 
+{
+    public static void main(String[] args)
+    {
+        int a=17;
+        int r=a--;
+        System.out.println(r);
+    }
+}
---
+++ Operators/increment.java
@@ -0,0 +1,10 @@
+//increment
+public class increment 
+{
+    public static void main(String[] args)
+    {
+        int a=10;
+        int r=a++;
+        System.out.println(r);
+    }
+}
