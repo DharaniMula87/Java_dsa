9d49eebd5d3f7ee8c315dbc532931eb6ba4e9a00
DharaniMula87 <dharanimula2006@gmail.com>
2026-08-08 12:08:54 +0530

    rigth shift

---
+++ Operators/leftshift.java
@@ -0,0 +1,10 @@
+//Left Shift
+public class leftshift 
+{
+    public static void main(String[] args)
+    {
+    int a=10;
+    int r=a<<1;
+    System.out.println(r);
+    }
+}
---
+++ Operators/rightshift.java
@@ -0,0 +1,10 @@
+//right shift
+public class rightshift 
+{
+    public static void main(String[] args)
+    {
+        int a=7;
+        int r=a>>2;
+        System.out.println(r);
+    }
+}
