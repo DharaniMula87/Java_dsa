c2bb58603a1187b148a1b4f9d02efdf156e11f2d
DharaniMula87 <dharanimula2006@gmail.com>
2026-08-08 10:10:53 +0530

    Compound assignment

---
+++ Operators/compound_ass.java
@@ -0,0 +1,12 @@
+public class compound_ass 
+{
+    public static void main(String[] args)
+    {
+        int a=12;
+        System.out.println("a+=2 : "+(a+=2));
+        System.out.println("a-=2 : "+(a-=2));
+        System.out.println("a*=2 : "+(a*=2));
+        System.out.println("a/=2 : "+(a/=2));
+        System.out.println("a%=2 : "+(a%=2));
+    }
+}
