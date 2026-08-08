b17b289c4915ebe0d1a6304c648f13ed7ee14d0a
DharaniMula87 <dharanimula2006@gmail.com>
2026-08-08 12:58:45 +0530

    smallest of 2 numbers

---
+++ Operators/smallestof2num.java
@@ -0,0 +1,11 @@
+//Smallest of two numbers
+public class smallestof2num 
+{
+    public static void main(String[] args)
+    {
+        int a=8;
+        int b=6;
+        int r=(a<b)?a:b;
+        System.out.println(r);
+    }    
+}
