e10af6ce64f7be04ebb5f74aa49d92b96b425586
DharaniMula87 <dharanimula2006@gmail.com>
2026-08-08 08:46:21 +0530

    modulus operator

---
+++ Operators/modulus.java
@@ -0,0 +1,10 @@
+public class modulus 
+{
+    public static void main(String[] args)
+    {
+        int a=74;
+        int b=3;
+        int r=a%b;
+        System.out.println("Reminder is "+r);
+    }   
+}
